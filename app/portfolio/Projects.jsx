c53bb9c2e499e2a90ff619ccedb1184dd66c9247
Muraina020 <glonal-murainaboluwatife@gmail.com>
2024-09-15 15:21:41 +0100

    first commit

--- app/portfolio/Projects.jsx
+++ app/portfolio/Projects.jsx
@@ -8,16 +8,16 @@ const Projects = ({projects}) => {
   return (
     <div className={styles.projects}>
         {
-    projects.map(({id, image, category, title, description, demo, github}) => <Card key={id} className={styles.projects}>
+    projects.map(({id, image, category, title, desc, demo, github}) => <Card key={id} className={styles.project}>
         <div className={styles.image}>
             <Image src={image} alt=''/>
         </div>
         <div className={styles.content}>
             <h4>{title}</h4>
-            <p>{description}</p>
+            <p>{desc}</p>
             <div className={styles.cta}>
           <Link href={demo} className={`${styles.btn} ${styles.primary} `} target='_blank' rel='noopener noreferrer'>Demo</Link>
-          <Link href={github} className={`${styles.btn} ${styles.primary} `} target='_blank' rel='noopener noreferrer'>Github</Link>
+          <Link href={github} className={`${styles.btn}`} target='_blank' rel='noopener noreferrer'>Github</Link>
             </div>
         </div>
     </Card>)
--- app/portfolio/data.js
+++ app/portfolio/data.js
@@ -4,7 +4,11 @@ import Image1 from "../assets/project1.png"
 import Image2 from "../assets/project2.png"
 import Image3 from "../assets/project3.png"
 import Image4 from "../assets/project4.png"
-
+import Image5 from "../assets/project5.png"
+import Image6 from "../assets/project6.png"
+import Image7 from "../assets/project7.jpg"
+import Image8 from "../assets/project8.png"
+// import { data } from "../services/servicesData"
 
 export const data = [
     {
@@ -16,17 +20,17 @@ export const data = [
         github: "https://github.com/Muraina020"},
     {
         id: 2, 
-        category: "Frontend developemnt", 
+        category: "Frontend", 
         image: Image2, 
         title: "Project title (portfolio)", 
-        desc: "A single page portfolio website ", 
+        desc: "A single page portfolio website with full framer motion and functional form page where users and make enquiry by sending direct message to the user. ", 
         demo: "https://greatstack.in/", 
         github: "https://github.com/Muraina020"},
 
     {id: 3, 
-   category: "Frontend developemnt", 
+   category: "Frontend", 
    image: Image3, title: "Project title1 (portfolio)",
-   desc: "A multipage page portfolio website ", 
+   desc: "A multipage website built with next.js where one can upload contents and images. It has a login and register page to authenticate users and also has single details page. It is a mantastic website", 
    demo: "https://next-js-app-five-gray.vercel.app/", 
    github: "https://github.com/Muraina020"},
     
@@ -35,7 +39,35 @@ export const data = [
         category: "fullstack", 
         image: Image4, 
         title: "Project title1 (Booking web application)", 
-        desc: "Hotel booking web appliction", 
+        desc: "Hotel booking web appliction is an application where one can actually make an online room reservstion. It is built with react.js, express.js and node js for the backend and non-relational Mongo Db database. It has a modal where on can check if a room actually available or occupied before payment can be made. It has payment integration (paystack).", 
+        demo: "https://greatstack.in/", github: "https://github.com/Muraina020"},
+    {
+        id: 5, 
+        category: "frontend", 
+        image: Image5, 
+        title: "Project title1 (Booking web application)", 
+        desc: "Hotel booking web appliction is an application where one can actually make an online room reservstion. It is built with react.js", 
+        demo: "https://greatstack.in/", github: "https://github.com/Muraina020"},
+    {
+        id: 6, 
+        category: "backend", 
+        image: Image6, 
+        title: "Project title1 (Booking web application)", 
+        desc: "Hotel booking web appliction is an application where one can actually make an online room reservstion. It is built with react.js, express.js and node js for the backend and non-relational Mongo Db database. It has a modal where on can check if a room actually available or occupied before payment can be made. It has payment integration (paystack).", 
+        demo: "https://greatstack.in/", github: "https://github.com/Muraina020"},
+    {
+        id: 7, 
+        category: "fullstack", 
+        image: Image7, 
+        title: "Project title1 (Booking web application)", 
+        desc: "Hotel booking web appliction is an application where one can actually make an online room reservstion. It is built with react.js, express.js and node js for the backend and non-relational Mongo Db database. It has a modal where on can check if a room actually available or occupied before payment can be made. It has payment integration (paystack).", 
+        demo: "https://greatstack.in/", github: "https://github.com/Muraina020"},
+    {
+        id: 8, 
+        category: "frontend", 
+        image: Image8, 
+        title: "Project title1 (Portfolio website)", 
+        desc: "A single page portfolio website with full framer motion and functional form page where users and make enquiry by sending direct message to the user.", 
         demo: "https://greatstack.in/", github: "https://github.com/Muraina020"},
 
    
