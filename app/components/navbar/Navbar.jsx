194969031341b48b6ec79b2d7868b55df0ab01fb
Muraina020 <glonal-murainaboluwatife@gmail.com>
2024-09-16 06:50:06 +0100

    my commit

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 "use client"
 
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import styles from './navbar.module.css'
 import Link from 'next/link'
 import {FaBars} from 'react-icons/fa6'
 import {AiOutlineClose} from 'react-icons/ai'
 import {GoMoon, GoSun} from 'react-icons/go'
 import { usePathname } from 'next/navigation'
+import { ThemeContext } from '../../context/themeContext'
 
 const data = [
     {id: 1, link: '/', caption: 'Home'},
@@ -18,8 +19,8 @@ const data = [
 
 const Navbar = () => {
   const [navOpen, setNavOpen] = useState(false)
-
-  const pathname = usePathname()
+  const pathname = usePathname();
+  const {theme, setTheme} = useContext(ThemeContext)
 
 //   close dropdown when on small screen
  const closeNavHandler = () => {
@@ -34,6 +35,17 @@ const Navbar = () => {
     }
  }, [navOpen])
 
+
+//  Function to change the theme
+
+  const changeThemeHandler = () =>{
+    if(theme === "light"){
+      setTheme('dark')
+    }else {
+      setTheme('light')
+    }
+  }
+
   return (
     <div className={styles.nav}>
         <div className={`container ${styles.navContainer}`}>
@@ -46,7 +58,7 @@ const Navbar = () => {
          }
         </ul>}
         <div className={styles.navBtns}>
-         <button className={styles.themeBtn}><GoMoon/></button>
+         <button className={styles.themeBtn} onClick={ changeThemeHandler}>{ theme === 'light' ? <GoMoon/> : <GoSun/>}</button>
          <button className={styles.navBtn} onClick={() => setNavOpen(!navOpen)}>{navOpen ? <AiOutlineClose/> : <FaBars/>}</button>
         </div>
         </div>
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar"
 import Footer from "./components/footer/Footer"
+import Main from "./Main";
+import { ThemeProvider } from "./context/themeContext";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,11 +25,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-       <main>
+      <ThemeProvider>
+       <Main>
        <Navbar/>
         {children}
         <Footer/>
-       </main>
+       </Main>
+       </ThemeProvider>
       </body>
     </html>
   );
